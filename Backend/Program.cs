967a951e8ce81d619d448f83692d0bba5c0005cf
Chris McGinn <Chris.McGinn@delinea.com>
2024-05-08 06:42:58 -0400

    use config, not working it dont yarp good with ngrok

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,11 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+
+
 builder.WebHost.UseTunnelTransport(o =>
 {
     o.MaxConnectionCount = 1;
     o.Transport = TransportType.HTTP2;
 });
+
+// This is the HTTP/2 endpoint to register this app as part of the cluster endpoint
+var url = builder.Configuration["Tunnel:Url"]!;
 
 builder.WebHost.ConfigureKestrel(o =>
 {
@@ -17,7 +22,8 @@ builder.WebHost.ConfigureKestrel(o =>
     // o.Listen(new UriEndPoint(new("https://localhost:7244/connect-ws")));
 
     // H2
-    o.Listen(new UriEndPoint(new("https://localhost:7244/connect-h2?clusterId=alpha")));
+    o.Listen(new UriEndPoint(new(url)));
+// o.Listen(new UriEndPoint(new(url)));
 });
 
 var app = builder.Build();
