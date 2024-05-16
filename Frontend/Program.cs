8f20dd7a3e844475af8d2ef8a702e792090c6e47
Chris McGinn <Chris.McGinn@delinea.com>
2024-05-16 09:23:05 -0400

    allow alt schemes

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,5 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.AllowAlternateSchemes = true;
+})
 builder.Services.AddReverseProxy()
        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
